3cfb44eb10aa31c05264757696a164e202c78768
Constance Mann <connie.mann@yahoo.com>
2021-02-02 18:16:55 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,11 +1,5 @@
 # code the #greeting method here!
-def greeting
-  puts "to be_truthy"
-end
-
 def greeting(name)
   puts "Hello, #{name}!"
 end
 
-greeting = "name"
-puts "Hello, #{name}. It's nice to meet you."
